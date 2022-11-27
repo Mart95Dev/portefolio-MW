f93eb56a641cf11d659605713476fb83da306623
Martial Wehrling <wehrling@free.fr>
2022-11-27 16:25:26 +0300

    ✨ T01> PAGES : mofify code component and refacto code

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 export default function Logo({ width }) {
   return (
-    <div className="flex items-center uppercase gap-1">
+    <div className="flex items-center uppercase gap-2 pl-4">
       <img
         className={width}
         src="/logo-Martial-Wehrling-Freelance-.png"
         alt="Freelance Front End Developper"
       />
-      <p>martial wehrling</p>
     </div>
   );
 }
---
+++ src/components/cardWorks/CardWorks.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CardWorks() {
+  return (
+    <div className="flex">
+      <div class="card">Martial Wehrling portefolio</div>
+    </div>
+  );
+}

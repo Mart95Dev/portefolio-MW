ac642fe6abefb40a739bc1dcd863c85cfd7e3053
Martial Wehrling <wehrling@free.fr>
2022-11-27 16:56:48 +0300

    ✨ T01>HOME : modify code style responsive

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,20 +8,20 @@ export default function Home() {
         <p className="pt-16 pl-10 text-lg md:text-xl 2xl:text-2xl">
           Bonjour, je suis freelance développeur Front-End React.
         </p>
-        <p className="pl-10 text-lg 2xl:text-2xl">
+        <p className="pl-10 text-lg md:text-xl 2xl:text-2xl">
           Bienvenue sur mon portefolio !
         </p>
         <div className="flex flex-wrap item-center justify-between">
-          <div className="mt-20 ml-6 md:ml-20 lg:ml-16">
+          <div className="mt-20 ml-6 md:ml-20 lg:ml-11 lg:mt-32 ">
             <img
-              className="w-[140%]] xl:w-[100%] xl:mt-16 2xl:w-[140%]"
+              className="w-[140%]] lg:w-[80%] xl:w-[80%]"
               src="/martial_wehrling.png"
               alt="Texte martial wehrling freelance Front developer"
             />
           </div>
-          <div className=" w-[80%] ml-16 pr-5 sm:ml-28 md:ml-28 xl:w-[36%] xl:pr-5 mt-20 mr-6">
+          <div className="w-[80%] ml-16 pr-5 sm:ml-28 md:ml-28 lg:ml-16 lg:mt-32 lg:pr-10 lg:w-[26%] xl:ml-16 xl:w-[43%]">
             <img
-              className=" h-80 rounded-tl-[36px] rounded-br-[36px] md:h-96 lg:h-96 xl:h-80 xl:rounded-full 2xl:w-[90%] 2xl:h-96"
+              className="h-80 rounded-tl-[36px] rounded-br-[36px] md:h-96 lg:h-48 lg:w-58"
               src="/dev_code.jpg"
               alt="ecran ordinateur avec vs code et du javascript"
             />
